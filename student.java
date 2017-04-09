eb02c8d8bdd25cc944306b9ff3c6f92514b52341
Elisha Clerigo <erclerigo@ucdavis.edu>
2017-04-08 21:40:49 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Student {
+        
+    // the Student class has
+    // one field
+    public int id;
+        
+    // the Student class has
+    // one constructor
+    public Student(int inputID) {
+        id = inputID;
+    }
+        
+    // the Student class has
+    // two methods
+    public void setId(int newValue) {
+        id = newValue;
+    }
+        
+    public int getId() {
+    	return id;
+    }
+        
+       
+
+
+//static Map<Integer, Student> map = new HashMap<Integer,Student>();
+
+//   public Map<Integer, Student> buildMap(Student[]students){
+//              for(Student s: students) map.put(s.getId(),s);
+//              return map;
+//              }
+}
+
+
+int main(){
+
+static Map<Integer, Student> map = new HashMap<Integer,Student>();  // declare a hashmap
+
+   public Map<Integer, Student> buildMap(Student[]students){        //declares a function buildMap that takes 
+              for(Student s: students) map.put(s.getId(),s);        // and iterates through all of them and puts each of them into the map with an id
+              return map;
+              }
+
+
+
+}
+
+
+
