81f21b94d5e39e300939e340654be0d21d445e2c
Elisha Clerigo <lisha1294@users.noreply.github.com>
2017-04-09 16:05:14 -0700

    Update StudentMap.java

--- StudentMap.java
+++ StudentMap.java
@@ -33,7 +33,7 @@ public Map<Integer, Student> buildMap(Student[]students){        //declares a fu
 
 
 	StudentMap m = new StudentMap();
-	Map<Integer, Student> m2 = buildMap(students); 
+	Map<Integer, Student> m2 = m.buildMap(students); 
 	 
 
 	//m.buildMap(students); //where did you declare buildMap? // answer: Map.java
