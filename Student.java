4fa4bfc2ce9c4727e095f941ff18fad092bf1c7d
Elisha Clerigo <lisha1294@users.noreply.github.com>
2017-04-21 19:15:10 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,24 +1,15 @@
 // see page72.txt for inspiration of code
 import java.util.*;
 
-
-
 public class Student {
 
-
-
     // the Student class has
-
     // one field
 
     public int id;
-
     private String name;
 
-
-
     // the Student class has
-
     // one constructor
 
     public Student(int inputID, String name) {  // you need to capitalize string
@@ -31,43 +22,26 @@ public class Student {
 
 
     // the Student class has
-
-    // two methods
+    // three methods
 
     public void setId(int newValue) {
-
         id = newValue;
-
-    }
-
-
+   }
 
     public int getId() {
-
     	return id;
-
     }
-
 
 
     public void setName(String newName){   // you need to capitalize string
-
-        name = newName;
-
+       name = newName;
     }
 
 
 
     public String getName(){// you need to capitalize string
-
     return name;
 
     }
-
-
-
-
-
-
 
 }
