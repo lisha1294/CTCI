545aa05413cd5ac3bf606abebd41d660d4da327a
Elisha Clerigo <lisha1294@users.noreply.github.com>
2017-04-26 12:31:24 -0700

    correct answer - hackerrank

--- 2Darray.cpp
+++ 2Darray.cpp
@@ -24,30 +24,8 @@
 using namespace std;
 
 
-int read(vector< vector<int> > arr;){
-//read an hourglass
-    int sum1, sum = 0;            // first row, middle, and third row
-   
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            sum1+=arr[i][j]
-        }   
-    }
-    
-    sum = sum1 - (arr[i+1][j] + arr[i+1][j+2]); // subtracting the extra from the middle
-    return sum;
-}
+  
 
-void print(vector< vector<int> > arr(3,vector<int>(3));){
-//read an hourglass
-    int sum1, sum = 0;            // first row, middle, and third row
-   
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-           cout << arr[i][j]; 
-        }   
-    }
-}
 
 int main(){
     vector< vector<int> > arr(6,vector<int>(6));
@@ -56,16 +34,37 @@ int main(){
           cin >> arr[arr_i][arr_j];
        }
     }
+
+  int sum1 =0;
+  int sum =0;
+  int max = -64;
+  int i =0;  
+  int j = 0;  
+  int k =0;
+  int l = 0;
     
     for(int arr_k = 0;arr_k < 4;arr_k++){
        for(int arr_l = 0;arr_l < 4;arr_l++){
-            int sum, max = 0;
-            sum = read(arr[arr_k][arr_l]);
-            if(sum > max) max = sum;
-            print(arr[arr_k][arr_l]);
+                    //summing an hourglass
+                     sum =0;
+                     sum = arr[arr_k][arr_l] +  arr[arr_k][arr_l+1] + arr[arr_k][arr_l+2] + arr[arr_k+1][arr_l+1] +    
+                         arr[arr_k+2][arr_l] +  arr[arr_k+2][arr_l+1] + arr[arr_k+2][arr_l+2];
+             
+            if(sum > max) 
+                {max = sum;
+                  sum =0; 
+                 
+                   //break;
+                //for(int k=arr_k; k<arr_k+3; k++){
+                //    for(int k=arr_l; k<arr_l+3; k++){
+                //    cout << arr[k][l]; 
+                //        }   
+                //    }
+                }  
+            
        }
+       
     }
-        
+     cout << max;
     return 0;
 }
-
